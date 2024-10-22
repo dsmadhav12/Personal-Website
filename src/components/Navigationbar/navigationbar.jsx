1bacf6e256f529a5bcd6c41194a4d18210f649e0
dsmadhav12 <dsmadhav@hotmail.com>
2024-10-22 15:01:59 -0400

    adding an image

--- src/components/Navigationbar/navigationbar.jsx
+++ src/components/Navigationbar/navigationbar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
 import styles from "./Navigationbar.module.css";
-
+import React, {useState} from "react";
+import {getImageUrl} from "../../utils";
 export const Navigationbar =() => {
     return ( 
     <nav className={styles.navigationbar}>
         <a className={styles.title} href="/">Home</a>
         <div className={styles.menu}>
+            <img className={styles.menuButton} src={getImageUrl("")}/>
         <ul className={styles.menuItems}>
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experience</a></li>
