331030f304afdefd002e03910bdc4fcdbdfacfa7
dsmadhav12 <dsmadhav@hotmail.com>
2024-10-22 19:08:58 -0400

    fixed about me bugs and worked on styling

--- src/components/Navigationbar/AboutMe/AboutMe.jsx
+++ src/components/Navigationbar/AboutMe/AboutMe.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { getImageUrl } from "../../../utils";
+import styles from "./AboutMe.module.css";
 
 export const AboutMe =() => {
-    return <section>
-        <div>
-            <h1>Hi, I'm Madhav Donepudi</h1>
-            <p>
+    return (<section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hello, I'm Madhav Donepudi</h1>
+            <p className={styles.description}>
                 I'm a 2nd year student at the University of Virginia pursuing a BS in Computer Science. 
                 I enjoy listening to music, golfing, and spending time with people. Contact me to learn more!
             </p>
-            <a href="mailto:dsmadhav@hotmail.com">Contact Me</a>
+            <a href="mailto:dsmadhav@hotmail.com" className={styles.contactButton}>Contact Me</a>
         </div>
-        <img src={getImageUrl("me.png")} alt="Image of myself" />
-    </section>;
-}
+        <img src={getImageUrl("me.png")} alt="Image of myself" className={styles.meIMG} />
+        <div className={styles.topBlur}/>
+        <div className={styles.bottomBlur}/>
+    </section>);
+};
