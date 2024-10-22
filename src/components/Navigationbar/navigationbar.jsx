0cefad1a2d168a8239699fc7df0d8f4d28ced1f0
dsmadhav12 <dsmadhav@hotmail.com>
2024-10-22 15:57:15 -0400

    worked on navbar drop down menu

--- src/components/Navigationbar/navigationbar.jsx
+++ src/components/Navigationbar/navigationbar.jsx
@@ -2,12 +2,18 @@ import styles from "./Navigationbar.module.css";
 import React, {useState} from "react";
 import {getImageUrl} from "../../utils";
 export const Navigationbar =() => {
+    const[menuOpen, setMenuOpen] = useState(false);
     return ( 
     <nav className={styles.navigationbar}>
         <a className={styles.title} href="/">Home</a>
         <div className={styles.menu}>
-            <img className={styles.menuButton} src={getImageUrl("")}/>
-        <ul className={styles.menuItems}>
+            <img className={styles.menuButton} src={
+                menuOpen
+                    ? getImageUrl("xIcon.png")
+                    : getImageUrl("menuIcon.png")} 
+                alt="menu-button"
+                onClick={()=> setMenuOpen(!menuOpen)}/>
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experience</a></li>
             <li><a href="#projects">Projects</a></li>
