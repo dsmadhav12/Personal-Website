f9916b74cb839e9db5d7d3ade69b4a1d123f7357
dsmadhav12 <dsmadhav@hotmail.com>
2024-10-24 00:40:35 -0400

    fixed issue with menu button when in full screen in the navigationbar.jsx file

--- src/components/Navigationbar/navigationbar.jsx
+++ src/components/Navigationbar/navigationbar.jsx
@@ -1,18 +1,35 @@
 import styles from "./Navigationbar.module.css";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {getImageUrl} from "../../utils";
 export const Navigationbar =() => {
+    const [fullScreen, setFullScreen] = useState(window.innerWidth > 930);
     const[menuOpen, setMenuOpen] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 930) {
+                setMenuOpen(false); 
+            }
+            setFullScreen(window.innerWidth > 930);
+        };
+
+        window.addEventListener("resize", handleResize); 
+
+        return () => {
+            window.removeEventListener("resize", handleResize); 
+        };
+    }, []);
     return ( 
     <nav className={styles.navigationbar}>
         <a className={styles.title} href="/">Home</a>
         <div className={styles.menu}>
+            {!fullScreen && (
             <img className={styles.menuButton} src={
                 menuOpen
                     ? getImageUrl("xIcon.png")
                     : getImageUrl("menuIcon.png")} 
                 alt="menu-button"
                 onClick={()=> setMenuOpen(!menuOpen)}/>
+            )}
         <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
         onClick={()=> setMenuOpen(false)}>
             <li><a href="#about">About</a></li>
