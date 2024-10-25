d20dcd5463478c5f901ca59d5568f457c3acb86c
dsmadhav12 <dsmadhav@hotmail.com>
2024-10-24 20:42:01 -0400

    finsihed the About section

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -3,44 +3,39 @@ import { getImageUrl } from "../../utils";
 import styles from "./AboutSection.module.css"
 export const AboutSection =() => {
     return (
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
         <h2 className={styles.title}>About</h2>
-        <div className={StyleSheet.content}>
-            <img
-            src = {getImageUrl("me2.png")}
-            alt="Graduation picture"
-            className={styles.gradIMG}
-            />
+        <div className={styles.content}>
             <ul className={styles.aboutItems}>
-                <li className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
                     <img
                     src = {getImageUrl("hand.png")}
                     alt="Hand waving picture"
                     />
-                    <div className={styles.aboutItemsText}>
+                    <div>
                         <h3>About Me</h3>
                         <p>I'm a 19-year-old student who graduated from Freedom High School in Loudoun County, Virginia. 
                             I enjoy watching and playing sports, such as football, basketball, and golf. 
                             I also enjoy spending time with friends, family, and my dog.</p>
                     </div>
                 </li>
-                <li className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
                     <img
                     src = {getImageUrl("laptop.png")}
                     alt="laptop picture"
                     />
-                    <div className={styles.aboutItemsText}>
+                    <div>
                         <h3>Software Development</h3>
                         <p>I'm a software engineer skilled in Java, Python, JavaScript, and more. 
                             Currently, I'm interning at Perceptron Labs, while working on projetcs to improve my skills.</p>
                     </div>
                 </li>
-                <li className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
                     <img
                     src = {getImageUrl("brain.png")}
                     alt="brain picture"
                     />
-                    <div className={styles.aboutItemsText}>
+                    <div>
                         <h3>Problem Solver</h3>
                         <p> I'm a problem solver who thrives on tackling challenges and finding efficient solutions to complex problems. 
                             Always eager to learn and grow with each new opportunity. </p>
